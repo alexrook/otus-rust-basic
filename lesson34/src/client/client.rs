2776c31b208f27ff1101e3870002a60b974cf148
Alex Grachev <alex.f.grach@gmail.com>
2024-12-27 14:27:19 +0000

    client added

--- lesson34/src/client/client.rs
+++ lesson34/src/client/client.rs
@@ -1,16 +1,86 @@
-use core::{core::Account, protocol::*, ser_de::*};
+use common::core::{NonZeroMoney, Operation};
+use common::protocol::*;
+use std::fmt::Debug;
+use std::{
+    io::{Error, ErrorKind, Result as IOResult, Write},
+    net::TcpStream,
+};
 
-fn main() {
-    println!("Client Works");
-    let initial = Protocol::Response(Ok(vec![Account {
-        account_id: "acc1".to_string(),
-        balance: 42,
-    }]));
-    let mut writer: Vec<u8> = Vec::new();
-    let actual: Result<(), String> = IO::write(&mut writer, &initial);
-    assert!(actual.is_ok());
+fn close<'a, E>(stream: &'a mut TcpStream) -> IOResult<()>
+where
+    E: From<String> + Into<String> + Debug,
+{
+    IO::write::<E>(stream, &Protocol::Quit)
+        .map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
+    let _ = stream.flush()?;
+    let _ = stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(())
+}
 
-    let deserialized: DesResult<Protocol, String> = Protocol::deserialize(&writer);
-    assert!(deserialized.is_ok());
-    assert_eq!(initial, deserialized.unwrap().0);
+fn handle_connection<E>(stream: &mut TcpStream, requests: Vec<Protocol>) -> IOResult<()>
+where
+    E: From<String> + Into<String> + Debug,
+{
+    for req in requests {
+        println!("Sending request[{:?}] to server", req);
+        IO::write::<E>(stream, &req).map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
+
+        let response =
+            IO::read::<E>(stream).map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
+
+        match response {
+            Protocol::Quit => {
+                println!("Quit command received");
+                return close::<E>(stream);
+            }
+            Protocol::Response(ret) => match ret {
+                Ok(accs) => {
+                    println!("The server returned a successful response.[{:?}]", accs);
+                }
+                Err(err) => {
+                    eprintln!("An error[{:?}] occurred while server negotiation", err);
+                }
+            },
+            Protocol::Request(_) => {
+                eprintln!("Unexpected server behavior, it should only send Response and Quit protocol commands");
+                return close::<E>(stream);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> IOResult<()> {
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8080") {
+        println!("Connected to the server");
+        let commands = vec![
+            Protocol::Request(Operation::Create("acc1".to_string())),
+            Protocol::Request(Operation::GetBalance("acc1".to_string())),
+            Protocol::Request(Operation::Deposit(
+                "acc1".to_string(),
+                NonZeroMoney::new(42).unwrap(),
+            )),
+            Protocol::Request(Operation::Withdraw(
+                "acc1".to_string(),
+                NonZeroMoney::new(12).unwrap(),
+            )),
+            Protocol::Request(Operation::GetBalance("acc1".to_string())),
+            Protocol::Request(Operation::Create("acc2".to_string())),
+            Protocol::Request(Operation::Move {
+                from: "acc1".to_string(),
+                to: "acc2".to_string(),
+                amount: NonZeroMoney::new(12).unwrap(),
+            }),
+            Protocol::Request(Operation::GetBalance("acc1".to_string())),
+            Protocol::Request(Operation::GetBalance("acc2".to_string())),
+            Protocol::Quit,
+        ];
+
+        handle_connection::<String>(&mut stream, commands)
+    } else {
+        let msg = "Couldn't connect to server";
+        eprintln!("{}", msg);
+        Err(Error::new(ErrorKind::BrokenPipe, msg))
+    }
 }
