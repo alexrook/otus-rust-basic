ff4fb5e624233e81375e28ecd96c128083f5830e
Alex Grachev <alex.f.grach@gmail.com>
2025-01-26 09:29:37 +0000

    v1:matrices indeces

---
+++ lesson38/src/lib/matrices.rs
@@ -0,0 +1,208 @@
+use std::{
+    iter::Product,
+    ops::{Add, Index, Mul}
+};
+
+use crate::matrix::Matrix;
+
+pub struct Matrices<'a, T, const M: usize, const N: usize>(Vec<&'a Matrix<T, M, N>>);
+
+//Эти две реализации Index компилируюся, но не работают как ожидалось
+//see test_index_should_work
+impl<'a, T, const M: usize, const N: usize> Index<usize> for Matrices<'a, T, M, N> {
+    type Output = Matrix<T, M, N>;
+
+    fn index(&self, index: usize) -> &'a Self::Output {
+        self.0.index(index)
+    }
+}
+
+// impl<'a, 'b, T, const M: usize, const N: usize> Index<usize> for &'b Matrices<'a, T, M, N>
+// where
+//     'a: 'b,
+// {
+//     type Output = Matrix<T, M, N>;
+
+//     fn index(&self, index: usize) -> &'a Self::Output {
+//         self.0.index(index)
+//     }
+// }
+
+impl<'a, T, const M: usize, const N: usize> Matrices<'a, T, M, N> {
+    pub fn new(vec: Vec<&'a Matrix<T, M, N>>) -> Matrices<'a, T, M, N> {
+        Matrices(vec)
+    }
+
+    //не удалось реализовать через трайт Index -> error[E0597]: actual does not live long enough
+    //это метод рабочий
+    pub fn my_index<'b>(&'b self, index: usize) -> &'a Matrix<T, M, N>
+    where
+        'a: 'b,
+    {
+        self.0[index]
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<'a, T, const M: usize, const N: usize> Matrices<'a, T, M, N>
+where
+    T: Default + 'static,
+    for<'b> &'b T: Add<Output = T>,
+{
+    pub fn sum_el(self) -> T {
+        let mut ret = T::default();
+        for matrix in self.0 {
+            for el in matrix.into_iter() {
+                ret = &ret + el;
+            }
+        }
+        ret
+    }
+}
+
+impl<'a, T, const M: usize, const N: usize> Matrices<'a, T, M, N>
+where
+    T: Default + 'static,
+    for<'b> &'b T: Mul<Output = T>,
+{
+    pub fn mul_el(self) -> T {
+        let mut prev: &T = &T::default();
+        let mut buf = T::default();
+        let mut first_time = true;
+        for matrix in self.0 {
+            for el in matrix.into_iter() {
+                if first_time {
+                    prev = el;
+                    first_time = false;
+                } else {
+                    buf = prev * el;
+                    prev = &buf;
+                }
+            }
+        }
+
+        buf
+    }
+}
+
+impl<'a, T, const M: usize, const N: usize> Matrices<'a, T, M, N>
+where
+    T: 'static + Product<&'a T>,
+{
+    pub fn product_el(self) -> T {
+        let r = self.0.into_iter().flat_map(|matrix| matrix.into_iter());
+        r.product()
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+
+    use super::Matrices;
+    use crate::matrices::*;
+
+    #[test]
+    fn test_my_index_should_work() {
+        let array = [0, 1, 2, 3, 4, 5];
+
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let should_be_m1: &Matrix<i32, 2, 3>;
+        let should_be_m2: &Matrix<i32, 2, 3>;
+        {
+            let actual = Matrices::new(vec![&m1, &m2]);
+            should_be_m1 = actual.my_index(0);
+            should_be_m2 = actual.my_index(1);
+        }
+        assert_eq!(should_be_m1, &m1);
+        assert_eq!(should_be_m2, &m2)
+    }
+
+    #[ignore]
+    #[test]
+    fn test_index_should_work() {
+        let array = [0, 1, 2, 3, 4, 5];
+
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let should_be_m1: &Matrix<i32, 2, 3>;
+
+        {
+            let actual = Matrices::new(vec![&m1, &m2]);
+          //  should_be_m1 = actual.index(0); //error here
+                                            // should_be_m2 = &actual[1];
+        }
+       // assert_eq!(should_be_m1, &m1);
+    }
+
+    #[test]
+    fn test_sum_el_should_work() {
+        let array = [0, 1, 2, 3, 4, 5];
+        let should_be_sum: i32 = 2 * array.iter().sum::<i32>();
+        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let right: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+
+        let actual = Matrices::new(vec![&left, &right]);
+
+        let actual_sum = actual.sum_el();
+
+        assert_eq!(actual_sum, should_be_sum)
+    }
+
+    #[test]
+    fn test_mul_el_should_work() {
+        let array = [1, 2, 3, 4, 5, 6];
+        let should_be_mul: i32 = array.iter().fold(1, |acc, el| acc * *el * *el);
+        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let right: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let actual = Matrices::new(vec![&left, &right]);
+        let actual_mul = actual.mul_el();
+        assert_eq!(actual_mul, should_be_mul);
+
+        let should_be_mul: i32 = 1 * 4 * 3 * 4 * 5 * 6 * //m1
+                                 3 * 4 * 3 * 7 * 12 * 6;
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[1, 4, 3, 4, 5, 6]);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let actual = Matrices::new(vec![&m1, &m2]);
+        let actual_mul = actual.mul_el();
+        assert_eq!(actual_mul, should_be_mul);
+
+        let should_be_mul: i32 = 0; //bcs m1 has zero
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[0, 4, 3, 4, 5, 6]);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let m3: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let actual = Matrices::new(vec![&m1, &m2, &m3]);
+        let actual_mul = actual.mul_el();
+        assert_eq!(actual_mul, should_be_mul)
+    }
+
+    #[test]
+    fn test_product_should_work() {
+        let array = [1, 2, 3, 4, 5, 6];
+        let should_be_mul: i32 = array.iter().fold(1, |acc, el| acc * *el * *el);
+        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let right: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&array);
+        let actual = Matrices::new(vec![&left, &right]);
+        let actual_mul = actual.product_el();
+        assert_eq!(actual_mul, should_be_mul);
+
+        let should_be_mul: i32 = 1 * 4 * 3 * 4 * 5 * 6 * //m1
+                                 3 * 4 * 3 * 7 * 12 * 6;
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[1, 4, 3, 4, 5, 6]);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let actual = Matrices::new(vec![&m1, &m2]);
+        let actual_mul = actual.product_el();
+        assert_eq!(actual_mul, should_be_mul);
+
+        let should_be_mul: i32 = 0; //bcs m1 has zero
+        let m1: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[0, 4, 3, 4, 5, 6]);
+        let m2: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let m3: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[3, 4, 3, 7, 12, 6]);
+        let actual = Matrices::new(vec![&m1, &m2, &m3]);
+        let actual_mul = actual.product_el();
+        assert_eq!(actual_mul, should_be_mul)
+    }
+}
--- lesson38/src/lib/matrix.rs
+++ lesson38/src/lib/matrix.rs
@@ -5,19 +5,18 @@ use std::{
     usize,
 };
 
-///M строки,N столбцы
 fn for_each_m_n<F, const M: usize, const N: usize>(mut f: F)
 where
     F: FnMut(usize, usize) -> (),
 {
-    for i in 0..M {
-        for j in 0..N {
-            f(i, j)
+    for row/*строки*/ in 0..M {
+        for col/*столбцы*/ in 0..N {
+            f(row, col)
         }
     }
 }
 ///M строки,N столбцы
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Matrix<T, const M: usize, const N: usize>([[T; N]; M]);
 
 impl<T, const M: usize, const N: usize> Matrix<T, M, N> {
@@ -33,24 +32,20 @@ impl<T, const M: usize, const N: usize> Matrix<T, M, N>
 where
     T: Default,
 {
-    pub fn map<F, R>(&self, mut f: F) -> Matrix<R, M, N>
-    where
-        R: Default,
-        F: FnMut(usize, usize, &T) -> R,
-    {
-        let mut zero = Matrix::<R, M, N>::default();
-        for_each_m_n::<_, M, N>(|i, j| zero.0[i][j] = f(i, j, &self.0[i][j]));
-        zero
-    }
-
-    pub fn from_array<const L: usize>(array: [T; L]) -> Matrix<T, M, N>
+    pub fn from_array<const L: usize>(array: &[T; L]) -> Matrix<T, M, N>
     where
         T: Clone,
     {
+        const {
+            if L != M * N {
+                panic!("L != M * N");
+            }
+        }
+
         assert!(M * N == L);
         let mut ret = Self::default();
         //в данной матрице каждая строка M это массив 0..N
-        Self::default().map(|i, j, _| ret.0[i][j] = array[j + i * N].clone());
+        for_each_m_n::<_, M, N>(|row, col| ret.0[row][col] = array[col + row * N].clone());
 
         ret
     }
@@ -69,6 +64,17 @@ impl<T: 'static, const M: usize, const N: usize> IntoIterator for Matrix<T, M, N
                 .map(move |(col, el)| (row, col, el))
         });
 
+        Box::new(ret)
+    }
+}
+
+impl<'a, T: 'static, const M: usize, const N: usize> IntoIterator for &'a Matrix<T, M, N> {
+    type Item = &'a T;
+
+    type IntoIter = Box<dyn Iterator<Item = Self::Item> + 'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let ret = self.0.iter().flat_map(|row| row.iter());
         Box::new(ret)
     }
 }
@@ -181,29 +187,28 @@ pub mod tests {
 
     #[test]
     fn test_from_array() {
-        let m1 = Matrix::<i32, 2, 3>::from_array([1, 2, 3, 4, 5, 6]);
+        let m1 = Matrix::<i32, 2, 3>::from_array(&[1, 2, 3, 4, 5, 6]);
         //по индексу от 0,
         // строка 0, колонка 0
         assert!(m1.0[0][0] == 1);
         // строка 1, колонка 2
         assert!(m1.0[1][2] == 6);
-
-        println!("{:?}", m1)
+        println!("{:?}", m1);
     }
 
     #[test]
     fn test_into_iter() {
-        let initial: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array([1, 2, 3, 4, 5, 6]);
+        let initial: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[1, 2, 3, 4, 5, 6]);
         for (row, col, el) in initial {
             println!("row[{}],col[{}],el[{}]", row, col, el)
         }
 
-        let initial = Matrix::<&str, 2, 3>::from_array(["a", "b", "c", "d", "i", "f"]);
+        let initial = Matrix::<&str, 2, 3>::from_array(&["a", "b", "c", "d", "i", "f"]);
         for (row, col, el) in initial {
             println!("row[{}],col[{}],el[{}]", row, col, el)
         }
 
-        let initial = Matrix::<String, 2, 3>::from_array([
+        let initial = Matrix::<String, 2, 3>::from_array(&[
             "A".to_string(),
             "B".to_string(),
             "C".to_string(),
@@ -220,8 +225,8 @@ pub mod tests {
 
     #[test]
     fn test_add_should_work() {
-        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array([0, 1, 2, 3, 4, 5]);
-        let right: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array([0, 1, 2, 3, 4, 5]);
+        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[0, 1, 2, 3, 4, 5]);
+        let right: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[0, 1, 2, 3, 4, 5]);
 
         let actual: Matrix<i32, 2, 3> = left + right;
 
@@ -233,22 +238,16 @@ pub mod tests {
 
     #[test]
     fn test_mul_should_work() {
-        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array([0, 1, 2, 3, 4, 5]);
-        println!("{}", left);
-        let right: Matrix<i32, 3, 2> = Matrix::<i32, 3, 2>::from_array([0, 1, 2, 3, 4, 5]);
-        println!("{}", right);
-
+        let left: Matrix<i32, 2, 3> = Matrix::<i32, 2, 3>::from_array(&[0, 1, 2, 3, 4, 5]);
+        let right: Matrix<i32, 3, 2> = Matrix::<i32, 3, 2>::from_array(&[0, 1, 2, 3, 4, 5]);
         let actual: Matrix<i32, 2, 2> = left.mul(right);
-
         println!("{}", actual);
+        assert_eq!(actual, Matrix::<i32, 2, 2>::from_array(&[10, 13, 28, 40]));
 
-        let left = Matrix::<i32, 2, 3>::from_array([1, 2, 4, 2, 0, 3]);
-        println!("{}", left);
-        let right = Matrix::<i32, 3, 2>::from_array([2, 5, 1, 3, 1, 1]);
-        println!("{}", right);
-
+        let left = Matrix::<i32, 2, 3>::from_array(&[1, 2, 4, 2, 0, 3]);
+        let right = Matrix::<i32, 3, 2>::from_array(&[2, 5, 1, 3, 1, 1]);
         let actual: Matrix<i32, 2, 2> = left.mul(right);
-
         println!("{}", actual);
+        assert_eq!(actual, Matrix::<i32, 2, 2>::from_array(&[8, 15, 7, 13]));
     }
 }
