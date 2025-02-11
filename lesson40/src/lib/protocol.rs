6796e9c8e29bd4b2cb0e931de98cc3f4e3fd5579
Alex Grachev <alex.f.grach@gmail.com>
2025-02-11 11:49:17 +0000

    serde + bincode with example added

--- lesson40/src/lib/protocol.rs
+++ lesson40/src/lib/protocol.rs
@@ -1,26 +1,87 @@
 use std::fmt::Debug;
 
-use crate::core::{Account, AccountId, NonZeroMoney, Operation};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum ClientRequest<'a> {
-    Create(&'a AccountId),                 //регистрация счёта
-    Deposit(&'a AccountId, NonZeroMoney),  //пополнение
-    Withdraw(&'a AccountId, NonZeroMoney), //снятие
+use crate::core::{AccountId, Money, NonZeroMoney};
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ClientRequest {
+    Create(AccountId),                 //регистрация счёта
+    Deposit(AccountId, NonZeroMoney),  //пополнение
+    Withdraw(AccountId, NonZeroMoney), //снятие
     Move {
         //перевод
-        from: &'a AccountId,
-        to: &'a AccountId,
+        from: AccountId,
+        to: AccountId,
         amount: NonZeroMoney,
     },
-    GetBalance(&'a AccountId),//получение баланса
-    Quit, //завершение сеанса
+    GetBalance(AccountId), //получение баланса
+    Quit,                  //завершение сеанса
 }
 
-pub enum ServerResponse{
-    AccountState(Account), //Create, Deposit, Withdraw, GetBalance ops response
-    FundsMovement{ //Move op response
-        from:Account,
-        to:Account
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AccountRef {
+    account_id: String,
+    balance: Money,
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ServerResponse {
+    AccountState(AccountRef), //Create, Deposit, Withdraw, GetBalance ops response
+    FundsMovement {
+        //Move op response
+        from: AccountRef,
+        to: AccountRef,
+    },
+    Error {
+        message: String,
+        code: i32,
+    },
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Debug;
+
+    use serde::{Deserialize, Serialize};
+
+    use crate::protocol::{AccountRef, ServerResponse};
+
+    use super::ClientRequest;
+
+    fn test_base<T>(message: T)
+    where
+        T: Serialize + Eq + Debug,
+        for<'a> T: Deserialize<'a>,
+    {
+        let encoded: Vec<u8> = bincode::serialize(&message).unwrap();
+        let actual: Result<T, bincode::Error> = bincode::deserialize(&encoded);
+        assert!(actual.is_ok());
+        assert_eq!(actual.unwrap(), message);
+    }
+
+    #[test]
+    fn test_client_marshalling() {
+        test_base(ClientRequest::Create("acc1".to_owned()));
+        test_base(ClientRequest::Quit);
+    }
+
+    #[test]
+    fn test_server_marshalling() {
+        test_base(ServerResponse::FundsMovement {
+            from: AccountRef {
+                account_id: "acc1".to_owned(),
+                balance: 120,
+            },
+            to: AccountRef {
+                account_id: "acc2".to_owned(),
+                balance: 42,
+            },
+        });
+
+        test_base(ServerResponse::Error {
+            message: "an error".to_owned(),
+            code: -123,
+        });
     }
 }
