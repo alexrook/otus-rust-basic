f531ad99543cfe05d03d53910db407db7115d89e
Alex Grachev <alex.f.grach@gmail.com>
2025-02-18 19:43:46 +0000

    server37 impl with ClientRequest, ServerResponse and log

--- lesson37/src/bin/server37.rs
+++ lesson37/src/bin/server37.rs
@@ -1,8 +1,9 @@
+use common::core::Account;
 use common::core::{Bank, InMemoryOpsStorage, InMemoryState, Operation, OpsStorage, State};
-use common::protocol::{read_proto, write_proto};
-use common::{core::Account, protocol::Protocol};
+use common::protocol::{AccountRef, ClientRequest, ServerResponse};
+use ftail::Ftail;
 use std::fmt::Debug;
-use std::io::{self, Error, ErrorKind, Write};
+use std::io::{self, Error, ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -11,10 +12,31 @@ fn close<E>(stream: &mut TcpStream) -> io::Result<()>
 where
     E: From<String> + Into<String> + Debug,
 {
-    write_proto::<E>(stream, &Protocol::Quit)
-        .map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
+    write_response(stream, &ServerResponse::Bye)
+        .map_err(|e| Error::new(ErrorKind::BrokenPipe, e))?;
     stream.flush()?;
     stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(())
+}
+
+fn read_request(stream: &mut TcpStream) -> io::Result<ClientRequest> {
+    let mut size_buf = [0u8; 4]; // Буфер для длины
+    stream.read_exact(&mut size_buf)?; // Читаем ровно 4 байта
+    let size = u32::from_be_bytes(size_buf) as usize; // Получаем размер пакета
+
+    let mut data_buf = vec![0; size];
+    stream.read_exact(&mut data_buf)?; // Читаем ровно `size` байтов
+
+    ClientRequest::deserialize(&data_buf).map_err(|e| Error::new(ErrorKind::BrokenPipe, e))
+}
+
+fn write_response(stream: &mut TcpStream, response: &ServerResponse) -> io::Result<()> {
+    let encoded: Vec<u8> = response
+        .serialize()
+        .map_err(|e| Error::new(ErrorKind::BrokenPipe, e))?;
+    let len = (encoded.len() as u32).to_be_bytes();
+    stream.write_all(&len)?;
+    stream.write_all(&encoded)?;
     Ok(())
 }
 
@@ -28,68 +50,86 @@ where
     E: From<String> + Into<String> + Debug,
 {
     loop {
-        let message =
-            read_proto::<E>(stream).map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
-
-        match message {
-            Protocol::Quit => {
-                println!("Quit command received");
+        let client_request: ClientRequest = read_request(stream)?;
+        log::debug!("Operation[{:?}] request received", client_request);
+        let op = match client_request {
+            ClientRequest::Quit => {
+                log::debug!("Quit command received");
                 break close::<E>(stream);
             }
-            Protocol::Response(_) => {
-                eprintln!("Unexpected client behavior, it should only send Request and Quit protocol commands");
-                break close::<E>(stream);
-            }
-            Protocol::Request(op) => {
-                println!("Operation[{:?}] request received", op);
-                let mut guard = bank
-                    .lock()
-                    .expect("It's called PoisonError, so I prefer to panic here");
+            ClientRequest::Create(account_id) => Operation::Create(account_id),
+            ClientRequest::Deposit(account_id, amount) => Operation::Deposit(account_id, amount),
+            ClientRequest::GetBalance(account_id) => Operation::GetBalance(account_id),
 
-                let response = match bank_deal(&mut *guard, op) {
-                    Ok(bank_accs) => {
-                        let cloned = Vec::from_iter(bank_accs.into_iter().cloned());
-                        Protocol::Response(Ok(cloned))
-                    }
-                    Err(bank_err) => {
-                        eprintln!("An error[{:?}] occurred while bank dealing", bank_err);
-                        Protocol::Response(Err(bank_err))
-                    }
-                };
+            ClientRequest::Move { from, to, amount } => Operation::Move { from, to, amount },
 
-                drop(guard);
+            ClientRequest::Withdraw(accout_id, amount) => Operation::Withdraw(accout_id, amount),
+        };
 
-                write_proto::<E>(stream, &response)
-                    .map_err(|e| Error::new(ErrorKind::BrokenPipe, e.into()))?;
-            }
-        }
+        let mut guard = bank
+            .lock()
+            .expect("It's called PoisonError, so I prefer to panic here");
+
+        let response = bank_deal(&mut *guard, op);
+        drop(guard);
+
+        write_response(stream, &response).map_err(|e| Error::new(ErrorKind::BrokenPipe, e))?;
     }
 }
 
-fn bank_deal<T, S>(bank: &mut Bank<T, S>, op: Operation) -> Result<Vec<&Account>, String>
+fn bank_deal<T, S>(bank: &mut Bank<T, S>, op: Operation) -> ServerResponse
 where
     T: OpsStorage,
     S: State,
 {
-    fn map_ret(ret: Result<&Account, String>) -> Result<Vec<&Account>, String> {
-        ret.map(|account| vec![account])
+    fn map_ret(ret: Result<&Account, String>) -> Result<ServerResponse, String> {
+        ret.map(|account| {
+            ServerResponse::AccountState(AccountRef {
+                account_id: account.account_id.clone(),
+                balance: account.balance,
+            })
+        })
     }
 
-    match op {
+    let ret = match op {
         Operation::Create(acc) => map_ret(bank.create_account(acc)),
         Operation::Deposit(acc, amount) => map_ret(bank.deposit(acc, amount)),
         Operation::Withdraw(acc, amount) => map_ret(bank.withdraw(acc, amount)),
         Operation::GetBalance(acc) => map_ret(bank.get_balance(&acc)),
         Operation::Move { from, to, amount } => {
-            bank.move_money(from, to, amount).map(Vec::from_iter)
+            bank.move_money(from, to, amount).and_then(|mut accounts| {
+                let from = accounts.next().ok_or(
+                    "the operation did not return the required number of elements".to_owned(),
+                )?;
+                let to = accounts.next().ok_or(
+                    "the operation did not return the required number of elements".to_owned(),
+                )?;
+                Ok(ServerResponse::FundsMovement {
+                    from: AccountRef {
+                        account_id: from.account_id.clone(),
+                        balance: from.balance,
+                    },
+                    to: AccountRef {
+                        account_id: to.account_id.clone(),
+                        balance: to.balance,
+                    },
+                    amount: amount,
+                })
+            })
         }
-    }
+    };
+    ret.unwrap_or_else(|e| ServerResponse::Error { message: e })
 }
 
 fn main() -> io::Result<()> {
+    Ftail::new()
+        .console(log::LevelFilter::max())
+        .init()
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
     let server = TcpListener::bind("127.0.0.1:8080")?;
 
-    println!("Server is listening on 127.0.0.1:8080");
+    log::debug!("Server is listening on 127.0.0.1:8080");
 
     let bank: Arc<Mutex<Bank<InMemoryOpsStorage, InMemoryState>>> = Arc::new(Mutex::new(
         Bank::new(InMemoryOpsStorage::default(), InMemoryState::default()),
@@ -99,22 +139,22 @@ fn main() -> io::Result<()> {
         match conn_result {
             Ok(mut stream) => {
                 let peer_addr = stream.peer_addr()?;
-                println!("Incoming connection from[{:?}]", peer_addr);
+                log::debug!("Incoming connection from[{:?}]", peer_addr);
                 let cloned_arc_bank = Arc::clone(&bank);
                 let _ = thread::spawn(move || {
                     match handle_connection::<InMemoryOpsStorage, InMemoryState, String>(
                         &mut stream,
                         cloned_arc_bank,
                     ) {
-                        Ok(()) => println!("Connection closed"),
+                        Ok(()) => log::debug!("Connection closed"),
                         Err(err) => {
-                            eprintln!("An error[{:?}] occured while connection handling", err)
+                            log::error!("An error[{:?}] occured while connection handling", err)
                         }
                     }
                 });
             }
             Err(e) => {
-                eprintln!("An error[{}] occurred while receiving tcp connection", e);
+                log::error!("An error[{}] occurred while receiving tcp connection", e);
             }
         }
     }
